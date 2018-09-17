ac186e1923851c2baed49e190769f027e23993b1
bhunor456 <balazs.hunoristvan@gmail.com>
2018-09-17 14:04:17 +0000

    Sensor&lcd

--- lcd.py
+++ lcd.py
@@ -16,8 +16,40 @@ LCD5_D7 = 5 #18-as GPIO láb, D7
 Echo = 20 # echo, GPIO 20-as láb
 Trig = 21 #trig, GPIO, 21-es láb
 
-GPIO.setup(Echo,GPIO.OUT)
-GPIO.setup(Trig,GPIO.OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+def distance ():
+    print("Mérés indul!")
+    GPIO.output(GPIO_TRIGGER, True)
+
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+
+    StartTime = time.time()
+    StopTime = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time
+
+    TimeElapsed = StopTime - StartTime 
+
+    distance = (TimeElapsed * 34000) / 2
+    return distance
+
+if __name__ == __'main'__:
+    try:
+        while True:
+            dist = distance()
+            print("Mért távolság %.1f cm" %dist)
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print("A mérés megszakadt, leáll")
+
 #HC_SR-04 szenzor
 
 
