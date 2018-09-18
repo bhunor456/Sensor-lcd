65312dd82527ac81d5b135b5f5ede9bbc10b116d
bhunor456 <balazs.hunoristvan@gmail.com>
2018-09-18 12:50:50 +0000

    sensor

--- sensor.py
+++ sensor.py
@@ -9,37 +9,44 @@ GPIO_ECHO = 21 #szürke kábel
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-def distance():
-    print ("Mérés indul!")
-    GPIO.output(GPIO_TRIGGER, True)
+print("Mérésre készen!")
 
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+#def distance():
+#print ("Mérés indul!")
 
+GPIO.output(GPIO_TRIGGER, False)
+print("Mérés indul!")
+time.sleep(2)
+
+GPIO.output(GPIO_TRIGGER, True)
+time.sleep(0.00001)
+GPIO.output(GPIO_TRIGGER, False)
+
+#StartTime = time.time()
+#StopTime = time.time()
+
+
+while GPIO.input(GPIO_ECHO) == 0:
     StartTime = time.time()
+
+while GPIO.input(GPIO_ECHO) == 1:
     StopTime = time.time()
-
-
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
     
-    TimeElapsed = StopTime - StartTime
+TimeElapsed = StopTime - StartTime
 
-    distance = (TimeElapsed * 34000) / 2
-    return distance
+distance = TimeElapsed * 17150
+distance = round(distance, 2)
+    #return distance
+print ("Távolság:"),distance,("cm")
 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Mért távolság = %.1f cm" % dist)
-            time.sleep(1)
-
-    except KeyboardInterrupt:
-        print("A mérés megszakadt, leáll")
+#if __name__ == '__main__':
+    #try:
+        #while True:
+            #dist = distance()
+            #print ("Mért távolság = %.1f cm" % dist)
+            #time.sleep(1)
+    #except KeyboardInterrupt:
+        #print("A mérés megszakadt, leáll")
 
 GPIO.cleanup()
 
